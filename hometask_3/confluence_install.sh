3b5a26dcfdbf7f1fa68afd205a178a3a9cb1e69d
Andrew <andriy.a.gordon@gmail.com>
2020-02-10 21:57:34 +0200

    fix few mistakes

--- hometask_3/confluence_install.sh
+++ hometask_3/confluence_install.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-if [ ! -e atlassian-confluence-7.3.1-x64.bin ]
+dir=/vagrant/
+
+if [ ! -e /vagrant/atlassian-confluence-7.3.1-x64.bin ]
 then
 	wget https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.3.1-x64.bin
+        dir=""
 fi
 
-echo "app.confHome=/var/atlassian/application-data/confluence7_3_1" > response.varfile
-echo "app.install.service$Boolean=true" >> response.varfile
-echo "portChoice=custom" >> response.varfile
-echo "httpPort$Long=8080" >> response.varfile
-echo "rmiPort$Long=8001" >> response.varfile
-echo "launch.application$Boolean=true" >> response.varfile
-echo "sys.adminRights$Boolean=true" >> response.varfile
-echo "sys.confirmedUpdateInstallationString=false" >> response.varfile
-echo "sys.installationDir=/opt/atlassian/confluence7_3_1" >> response.varfile
-echo "sys.languageId=en" >> response.varfile
+echo "app.confHome=/var/atlassian/application-data/confluence7_3_1" > "$dir"response.varfile
+echo "app.install.service$Boolean=true" >> "$dir"response.varfile
+echo "portChoice=custom" >> "$dir"response.varfile
+echo "httpPort$Long=8080" >> "$dir"response.varfile
+echo "rmiPort$Long=8001" >> "$dir"response.varfile
+echo "launch.application$Boolean=true" >> "$dir"response.varfile
+echo "sys.adminRights$Boolean=true" >> "$dir"response.varfile
+echo "sys.confirmedUpdateInstallationString=false" >> "$dir"response.varfile
+echo "sys.installationDir=/opt/atlassian/confluence7_3_1" >> "$dir"response.varfile
+echo "sys.languageId=en" >> "$dir"response.varfile
 
-chmod a+x atlassian-confluence-7.3.1-x64.bin
-./atlassian-confluence-7.3.1-x64.bin -q -varfile response.varfile
-rm response.varfile
+chmod a+x "$dir"atlassian-confluence-7.3.1-x64.bin
+"$dir"atlassian-confluence-7.3.1-x64.bin -q -varfile "$dir"response.varfile
+rm "$dir"response.varfile
 
-if [ ! -e 5.1.48.tar.gz ]
+if [ ! -e /vagrant/5.1.48.tar.gz ]
 then
 	wget https://github.com/mysql/mysql-connector-j/archive/5.1.48.tar.gz
+else
+	cp /vagrant/5.1.48.tar.gz $( pwd )
 fi
 
 tar xvzf 5.1.48.tar.gz mysql-connector-java-5.1.48/mysql-connector-java-5.1.48-bin.jar
